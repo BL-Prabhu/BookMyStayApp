a449b58d6624671cb161a0b7f018651a811c0e82
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-19 00:09:32 +0530

    FEAT : add Inventory Service Interface

--- BookMyStay/src/service/InventoryService.java
+++ BookMyStay/src/service/InventoryService.java
@@ -1,24 +1,12 @@
 package service;
 
-import model.RoomInventory;
 import model.RoomType;
-
-import java.util.List;
 import java.util.Map;
 
 public interface InventoryService {
-
-    void addRoomType(RoomType roomType, int count, double price);
-
-    void updateRoomCount(RoomType roomType, int newCount);
-
-    void updateRoomPrice(RoomType roomType, double newPrice);
-
-    RoomInventory getRoomDetails(RoomType roomType);
-
-    List<RoomInventory> getAllRooms();
+    void addRoomType(RoomType type, int count, double price);
+    void updateRoomCount(RoomType type, int count);
+    Map<RoomType, Integer> getRoomCounts();
 
     Map<RoomType, Double> getRoomPrices();
-
-    Map<RoomType, Integer> getRoomCounts();
 }
