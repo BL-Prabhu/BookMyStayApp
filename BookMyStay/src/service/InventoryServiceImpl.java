4fc624a3b2f22d859d8a843dfee5e6213e0b317f
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-19 00:11:06 +0530

    FEAT : add Inventory Service Implementation

--- BookMyStay/src/service/InventoryServiceImpl.java
+++ BookMyStay/src/service/InventoryServiceImpl.java
@@ -1,63 +1,27 @@
 package service;
 
-import exception.RoomNotFoundException;
-import model.RoomInventory;
 import model.RoomType;
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InventoryServiceImpl implements InventoryService {
 
-    private final Map<RoomType, RoomInventory> inventoryMap = new HashMap<>();
+    private final Map<RoomType, Integer> roomCounts = new HashMap<>();
 
-    @Override
-    public void addRoomType(RoomType roomType, int count, double price) {
-        inventoryMap.put(roomType, new RoomInventory(roomType, count, price));
+    public void addRoomType(RoomType type, int count, double price) {
+        roomCounts.put(type, count);
     }
 
-    @Override
-    public void updateRoomCount(RoomType roomType, int newCount) {
-        getRoom(roomType).setCount(newCount);
+    public void updateRoomCount(RoomType type, int count) {
+        roomCounts.put(type, count);
     }
 
-    @Override
-    public void updateRoomPrice(RoomType roomType, double newPrice) {
-        getRoom(roomType).setPrice(newPrice);
-    }
-
-    @Override
-    public RoomInventory getRoomDetails(RoomType roomType) {
-        return getRoom(roomType);
-    }
-
-    @Override
-    public List<RoomInventory> getAllRooms() {
-        return new ArrayList<>(inventoryMap.values());
+    public Map<RoomType, Integer> getRoomCounts() {
+        return new HashMap<>(roomCounts);
     }
 
     @Override
     public Map<RoomType, Double> getRoomPrices() {
-        Map<RoomType, Double> prices = new HashMap<>();
-        for (RoomInventory room : inventoryMap.values()) {
-            prices.put(room.getRoomType(), room.getPrice());
-        }
-        return prices;
-    }
-
-    @Override
-    public Map<RoomType, Integer> getRoomCounts() {
-        Map<RoomType, Integer> counts = new HashMap<>();
-        for (RoomInventory room : inventoryMap.values()) {
-            counts.put(room.getRoomType(), room.getCount());
-        }
-        return counts;
-    }
-
-    private RoomInventory getRoom(RoomType type) {
-        RoomInventory room = inventoryMap.get(type);
-        if (room == null) {
-            throw new RoomNotFoundException("Room not found: " + type);
-        }
-        return room;
+        return Map.of();
     }
 }
